7e90407f9f7e52efa9a61bc8919eb7ce162f2029
Aminu Musa <codecoder669@gmail.com>
2024-03-10 11:59:20 +0100

    HTML, CSS AND JAVASCRIPT AND IMAGES AS STATIC FILES ADDED

--- server.js
+++ server.js
@@ -8,16 +8,8 @@ let host = "localhost";
 
 //SERVER CREATED CREATED
 const server = http.createServer((req, res) => {
-
-
-
-
-
-
-
-
   // ---------------- HTML ---------------------
-  if (req.url === "/") {
+  if (req.url === "/" || req.url === "/index.html") {
     res.setHeader("Content-Type", "text/html");
     fs.readFile("./FrontEnd/index.html", "UTF-8", (err, home) => {
       if (err) {
@@ -26,8 +18,7 @@ const server = http.createServer((req, res) => {
         res.statusCode = 200;
         res.end(home);
       }
-    })
-
+    });
   } else if (req.url === "/about" || req.url === "/about.html") {
     res.setHeader("Content-Type", "text/html");
     fs.readFile("./FrontEnd/about.html", "UTF-8", (err, about) => {
@@ -37,8 +28,7 @@ const server = http.createServer((req, res) => {
         res.end(about);
       }
     });
-  }
-   else if (req.url === "/contact" || req.url === "/contact.html") {
+  } else if (req.url === "/contact" || req.url === "/contact.html") {
     res.setHeader("Content-Type", "text/html");
     fs.readFile("./FrontEnd/contact.html", "UTF-8", (err, contact) => {
       if (err) throw err;
@@ -47,66 +37,106 @@ const server = http.createServer((req, res) => {
         res.end(contact);
       }
     });
-  }
-   else{
+  } else if (req.url === "/services" || req.url === "/services.html") {
+    res.setHeader("Content-Type", "text/html");
+    fs.readFile("./FrontEnd/services.html", "UTF-8", (err, services) => {
+      if (err) throw err;
+      else {
+        res.statusCode = 200;
+        res.end(services);
+      }
+    });
+  } else if (req.url === "/projects" || req.url === "/projects.html") {
+    res.setHeader("Content-Type", "text/html");
+    fs.readFile("./FrontEnd/projects.html", "UTF-8", (err, projects) => {
+      if (err) throw err;
+      else {
+        res.statusCode = 200;
+        res.end(projects);
+      }
+    });
+  } else {
     res.setHeader("Content-Type", "text/html");
     fs.readFile("./FrontEnd/errorPage.html", "UTF-8", (err, errorPage) => {
-        res.statusCode = 200;
-        res.end(errorPage);
+      res.statusCode = 200;
+      res.end(errorPage);
     });
   }
 
-
-
   // -------------------CSS-------------------------
-if(req.url.match(/.css$/)){
-  // if file ends with .css
-  
-  // define the path of the css we want to render
-  let cssPath =  path.join(__dirname, "FrontEnd", req.url)
-  
-  // read the css file as a stream
-    let cssReadStream = fs.createReadStream(cssPath, "UTF-8" )
-  
-  // SETTING HEADERS FOR THE CSS FILE
+  if (req.url.match(/.css$/)) {
+    // if file ends with .css
+
+    // define the path of the css we want to render
+    let cssPath = path.join(__dirname, "FrontEnd", req.url);
+
+    // read the css file as a stream
+    let cssReadStream = fs.createReadStream(cssPath, "UTF-8");
+
+    // SETTING HEADERS FOR THE CSS FILE
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/css");
-  
+
     // PIPPING STREAM OF DATA TO THE MAIN RESPONSE
-    cssReadStream.pipe(res)
+    cssReadStream.pipe(res);
   }
+
+  // ------------------- IMAGES -------------------------
+
+  if (req.url.match(/.jpg$/)) {
+    let imagePath = path.join(__dirname, "FrontEnd", req.url);
+    let imageReadStream = fs.createReadStream(imagePath);
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "image/jpg");
+
+    imageReadStream.pipe(res);
+
+  }else if (req.url.match(/.png$/)){
+    let imagePath = path.join(__dirname, "FrontEnd", req.url);
+    let imageReadStream = fs.createReadStream(imagePath);
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "image/png");
+    imageReadStream.pipe(res);
+  }else if (req.url.match(/.svg$/)){
+    let imagePath = path.join(__dirname, "FrontEnd", req.url);
+    let imageReadStream = fs.createReadStream(imagePath);
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "image/svg+xml");
+    imageReadStream.pipe(res);
+  }else if (req.url.match(/.gif$/)){
+    let imagePath = path.join(__dirname, "FrontEnd", req.url);
+    let imageReadStream = fs.createReadStream(imagePath);
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "image/gif");
+    imageReadStream.pipe(res);
+  }else{
+    // res.end()
+  }
+
+
+
 
 
   // ------------------- JAVASCRIPT -------------------------
-  if(req.url.match(/.js$/)){
+  if (req.url.match(/.js$/)) {
+    // define the path of the css we want to render
+    let ScriptPath = path.join(__dirname, "FrontEnd", req.url);
 
-  // define the path of the css we want to render
-  let ScriptPath =  path.join(__dirname, "FrontEnd", req.url)
-  
-  // read the css file as a stream
-    let ScriptReadStream = fs.createReadStream(ScriptPath, "UTF-8" )
-  
-  // SETTING HEADERS FOR THE CSS FILE
+    // read the css file as a stream
+    let ScriptReadStream = fs.createReadStream(ScriptPath, "UTF-8");
+
+    // SETTING HEADERS FOR THE CSS FILE
     res.statusCode = 200;
     res.setHeader("Content-Type", "application/javascript");
-  
+
     // PIPPING STREAM OF DATA TO THE MAIN RESPONSE
-    ScriptReadStream.pipe(res)
-
+    ScriptReadStream.pipe(res);
   }
-
-
-
-
-
-
 });
-
-
-
-
-
-
 
 server.listen(port, host, () => {
   console.log(`Node server running @ port: http://${host}:${port}`);
